21d8a17fd5df425ae779c49f51cb66c5984db89b
Artem Trofymenko <randallsng@gmail.com>
2023-02-11 16:38:37 +0100

    updated app with simple authentication

---
+++ src/main/java/org/trainings/FirstSecurityApp/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.trainings.FirstSecurityApp.config;
+
+import org.hibernate.internal.build.AllowPrintStacktrace;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.trainings.FirstSecurityApp.security.AuthProviderImpl;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthProviderImpl authProvider;
+
+    @Autowired
+    public SecurityConfig(AuthProviderImpl authProvider) {
+        this.authProvider = authProvider;
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authProvider);// TODO
+    }
+
+}
---
+++ src/main/java/org/trainings/FirstSecurityApp/models/Person.java
@@ -0,0 +1,76 @@
+package org.trainings.FirstSecurityApp.models;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "username")
+    @NotEmpty(message = "username cant be empty")
+    @Size(min = 2, max = 100, message = "username should be between 2-100 characters")
+    private String username;
+    @Column(name = "year_of_birth")
+    @Min(value = 1950, message = "birth year should be above 1950")
+    @Max(value = 2023, message = "birth year should be below 2023")
+    private int yearOfBirth;
+    @Column(name = "password")
+    private String password;
+
+    public Person() {
+    }
+
+    public Person(String username, int yearOfBirth) {
+        this.username = username;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
