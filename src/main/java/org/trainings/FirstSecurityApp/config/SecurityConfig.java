2534b45dbaad07df0eed7e52043dad332ce96a5d
Artem Trofymenko <randallsng@gmail.com>
2023-02-11 17:34:01 +0100

    custom auth page implementation

--- src/main/java/org/trainings/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/org/trainings/FirstSecurityApp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.trainings.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,17 +19,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //here config spring security && authorization conf
 
-        try {
-            auth.userDetailsService(personDetailsService);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        http.csrf().disable()               //for turning off multi web request
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
+    //auth config
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
+    public PasswordEncoder getPasswordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
 }
