0e7c55560685cd348b18a0312077af1e62d6ae96
Artem Trofymenko <randallsng@gmail.com>
2023-02-11 12:46:05 +0100

    creating first security app

---
+++ src/main/java/org/trainings/FirstSecurityApp/FirstSpringBootAppWithSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.trainings.FirstSecurityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootAppWithSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootAppWithSecurityApplication.class, args);
+	}
+
+}
